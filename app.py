d03239316d28791c9f64517bf94f43098d75c538
kschell <kai.schell@lpice.eu>
2021-11-17 12:01:25 +0100

    Add two html templates and routes

--- app.py
+++ app.py
@@ -1,10 +1,17 @@
-from flask import Flask
+from flask import Flask, render_template
 
+# Flask Objekt erzeugen
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return "Ich bin eine richtig echte kleine Flask App im Debug Modus"
+    return render_template("index.html")
 
+@app.route("/test")
+def test():
+    return render_template("test.html")
+
+
+# 'Main Methode', Startpunkt der Applikation
 if __name__ == '__main__':
     app.run(debug=True)
