0740aae0126522a4fae30d0d0740fcde66ae0ec4
kschell <kai.schell@lpice.eu>
2021-11-17 15:40:56 +0100

    Add escaping for passed parameter

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template
+from markupsafe import escape
 
 # Flask Objekt erzeugen
 app = Flask(__name__)
@@ -8,9 +9,14 @@ def index():
     return render_template("index.html")
 
 
-@app.route("/test/<my_name>")
+# Wir können auch mehrere Routen für eine Funktion
+# angeben und zusätzlich Default Werte für Parameter
+# festlegen
+@app.route("/test/", defaults={'my_name': None})
+@app.route("/test/<string:my_name>")
 def test(my_name):
-    return render_template("test.html", my_name=my_name)
+    sum = my_name * 2
+    return render_template("test.html", my_name=escape(my_name))
 
 
 # 'Main Methode', Startpunkt der Applikation
