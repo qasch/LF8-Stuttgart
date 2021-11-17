b21a352d59a0fc2915385a38bda393daaca14a14
kschell <kai.schell@lpice.eu>
2021-11-17 11:21:19 +0100

    Add main function

--- app.py
+++ app.py
@@ -4,5 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return "Ich bin eine Flask App"
+    return "Ich bin eine richtig echte kleine Flask App"
 
+if __name__ == '__main__':
+    app.run()
