6d536067c1879bed60e55a81854d6aaac55b6a5b
kschell <kai.schell@lpice.eu>
2021-11-17 11:30:27 +0100

    Enable debug mode

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return "Ich bin eine richtig echte kleine Flask App"
+    return "Ich bin eine richtig echte kleine Flask App im Debug Modus"
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
